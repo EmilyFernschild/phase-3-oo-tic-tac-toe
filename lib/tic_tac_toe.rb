ffe813505c2e073b7bf0e9132996fce48f72f02b
Emily Fernschild <emilyfernschild@gmail.com>
2022-10-17 17:36:17 -0400

    commit tree 33f89c84fe6096684ca0ab6fa9951cd864b931c4

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,98 @@
+class TicTacToe
+    attr_accessor :board
+
+    WIN_COMBINATIONS = [
+        [0,1,2], 
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    def initialize
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+    end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(input)
+        input.to_i - 1
+    end
+
+    def move(index, token = "X")
+        @board[index] = token
+    end
+
+    def position_taken?(index)
+        @board[index] == " " ? false : true
+    end
+
+    def valid_move?(index)
+        @board[index] == " " ? true : nil
+    end
+
+    def turn_count
+       @board.count{|x| x != " " }
+    end
+    
+    def current_player
+        if turn_count.even?
+            return "X"
+        else 
+            return "O"
+        end
+    end
+
+    def turn
+        puts "Please enter a number (1-9):"
+        user_input = gets.strip
+        index = input_to_index(user_input)
+        if valid_move?(index)
+          token = current_player
+          move(index, token)
+        else
+            turn
+        end
+        display_board
+      end
+
+      def won?
+        WIN_COMBINATIONS.any? do |combo|
+          if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+            return combo
+          end
+        end
+      end
+
+      def full?
+        @board.all?{|x| x != " " }
+      end
+
+      def draw?
+        full? && !won?
+      end
+
+      def over?
+        full? || draw?
+      end
+
+      def winner
+        if combo = won?
+          @board[combo[0]]
+        end
+      end
+      
+      def play
+        turn until over?
+        puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+      end
+end
